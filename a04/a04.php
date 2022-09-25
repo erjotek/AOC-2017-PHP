67cf1487c9bd316a464b5576ab5edf3213c671e1
Rafał Janiczek <rafal.janiczek@sileman.pl>
2022-09-25 23:10:04 +0200

    2017 a04

---
+++ a04/a04.php
@@ -0,0 +1,39 @@
+<?php
+
+function part1($input)
+{
+    $input = explode("\n", $input);
+
+    return count(
+        array_filter(
+            $input,
+            fn($l) => explode(" ", $l) === array_unique(explode(" ", $l))
+        )
+    ); // 325
+}
+
+function part2($input)
+{
+    $input = explode("\n", $input);
+
+    return count(
+        array_filter(
+            $input,
+            function ($l) {
+                $l = explode(" ", $l);
+                $m = array_unique(
+                    array_map(function ($x) {
+                        $x = str_split($x);
+                        sort($x);
+
+                        return implode($x);
+                    }, $l)
+                );
+
+                return count($m) === count($l);
+            }
+        )
+    ); //119
+}
+
+include __DIR__ . '/../template.php';
