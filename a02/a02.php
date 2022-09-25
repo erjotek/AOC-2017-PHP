a4215662aee1c2927de31c875615ed6882c5ba93
Rafał Janiczek <rafal.janiczek@sileman.pl>
2022-09-25 10:27:36 +0200

    2017 a02

---
+++ a02/a02.php
@@ -0,0 +1,37 @@
+<?php
+
+function part1($input)
+{
+    return
+        array_sum(
+            array_map(fn($l) => max($l) - min($l),
+                array_map(fn($l) => explode("\t", $l),
+                    explode("\n", $input)
+                )
+            )
+        ); //36766
+}
+
+function part2($input)
+{
+    $input = explode("\n", $input);
+    $input = array_map(fn($l) => explode("\t", $l), $input);
+
+    $sum = 0;
+    $c = count($input[0]);
+    foreach ($input as $line) {
+        for ($i = 0; $i < $c - 1; $i++) {
+            for ($j = $i + 1; $j < $c; $j++) {
+                $d = max($line[$i], $line[$j]) / min($line[$i], $line[$j]);
+                if (is_int($d)) {
+                    $sum += $d;
+                    break 2;
+                }
+            }
+        }
+    }
+
+    return $sum; //261
+}
+
+include __DIR__ . '/../template.php';
