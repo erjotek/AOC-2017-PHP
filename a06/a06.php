bd01eed6ac4e462fa4303707352e612a5052ece3
Rafał Janiczek <rafal.janiczek@sileman.pl>
2022-09-30 00:06:39 +0200

    2017 a06 = speedup

--- a06/a06.php
+++ a06/a06.php
@@ -1,14 +1,15 @@
 <?php
 
-function part1($input, $second = false)
+$part2 = 0;
+
+function part1($input)
 {
     $input = explode("\t", $input);
     $c = count($input);
     $seen = [];
     $i = 0;
     while (!isset($seen[implode('-', $input)])) {
-        $i++;
-        $seen[implode('-', $input)] = true;
+        $seen[implode('-', $input)] = $i++;
         $key = (int)array_search(max($input), $input);
         $val = $input[$key];
         $input[$key] = 0;
@@ -18,16 +19,16 @@ function part1($input, $second = false)
         }
     }
 
-    if ($second) {
-        return part1(implode("\t", $input));
-    }
+    global $part2;
+    $part2 =  $i - $seen[implode('-', $input)];
 
     return $i; //12841
 }
 
 function part2($input)
 {
-    return part1($input, true); //8038
+    global $part2;
+    return $part2; //8038
 }
 
 include __DIR__ . '/../template.php';
