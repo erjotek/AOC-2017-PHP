1c20a72d3bc775f2e3e5eb36a780a6f3362964a4
Rafał Janiczek <rafal.janiczek@int.pl>
2025-01-26 09:09:10 +0100

    new template

--- template.php
+++ template.php
@@ -1,24 +1,25 @@
 <?php
 
-function display(int $part)
+ini_set('memory_limit', '4G');
+
+function _display(int $part): void
 {
     global $argv;
     $day = basename($argv[0], '.php');
 
-    $startMem = memory_get_peak_usage();
-    $startTs = microtime(true);
+    memory_reset_peak_usage();
+
+    $startTs = hrtime(true);
     $func = 'part' . $part;
+
     echo "== Day $day part $part ==" . PHP_EOL;
-    echo 'Result: ' . $func(input()) . PHP_EOL;
-    echo 'Time  : ' . sprintf('%.5f', microtime(true) - $startTs) . ' s' . PHP_EOL;
-    echo 'Memory: ' . sprintf('%.5f', ((memory_get_peak_usage() - $startMem) / 1024 /1024)) . ' MB' . PHP_EOL;
+    echo 'Result: ' . $func(input(rtrim(file_get_contents(__DIR__ . "/inputs/$day.txt")))) . PHP_EOL;
+    echo 'Time  : ' . sprintf('%.6f', (hrtime(true) - $startTs) / 1e+9) . ' s' . PHP_EOL;
+    echo 'Memory: ' . sprintf('%.5f', (memory_get_peak_usage() / 1024 ** 2)) . ' MB' . PHP_EOL;
     echo PHP_EOL;
 }
 
-function input(string $name = 'input')
-{
-    return rtrim(file_get_contents("$name.txt"));
-}
-
-display(1); /** @uses part1() */
-display(2); /** @uses part2() */
+/** @uses part1 */
+_display(1);
+/** @uses part2 */
+_display(2);
